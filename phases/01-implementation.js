4cf114b81da717140c1b9b1238c174905f573da4
seanjeyam <rohit.seanj@gmail.com>
2024-05-17 00:03:25 -0500

    Hash Tables phase 1D

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -6,11 +6,13 @@ class KeyValuePair {
   }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+  // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here 
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
   }
 
   hash(key) {
@@ -28,26 +30,83 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     return this.hash(key) % this.capacity;
   }
 
-
   insert(key, value) {
-    // Your code here 
+    // const index = this.hashMod(key);
+    // let current = this.data[index];
+
+    // const newPair = new KeyValuePair(key, value);
+    // if (!current) {
+    //   current = newPair;
+    //   this.count++;
+    // } else {
+    //   while (current) {
+    //     if (current.key === key) {
+    //       current.value = value;
+    //       return;
+    //     }
+    //     if (current.next === null) {
+    //       break;
+    //     } else {
+    //       current = current.next;
+    //     }
+    //   }
+
+    const index = this.hashMod(key);
+    let current = this.data[index];
+
+    // Handle updating value if key already exists
+    while (current) {
+      if (current.key === key) {
+        current.value = value; // Update the value
+        return;
+      }
+      current = current.next;
+    }
+
+    // Create new KeyValuePair and insert it at the beginning of the linked list
+    const newPair = new KeyValuePair(key, value);
+    newPair.next = this.data[index];
+    this.data[index] = newPair;
+    this.count++;
   }
 
+  // newPair.next = current;
+  // current = newPair;
+  // this.count++;
 
   read(key) {
-    // Your code here 
-  }
+    // variable for index given the key parameter
+    const index = this.hashMod(key);
+    // variable for value at index derived from key parameter
+    let current = this.data[index];
 
+    // loops through indexes on linked list
+    while (current) {
+      //identifies currentVal's key
+      if (current.key === key) {
+        return current.value;
+      }
+      current = current.next;
+    }
+
+    return undefined;
+  }
 
   resize() {
-    // Your code here 
+    // Your code here
+    return (this.capacity *= 2);
   }
 
-
   delete(key) {
-    // Your code here 
+    // Your code here
   }
 }
 
+// const hashTable = new HashTable(4);
+
+// hashTable.insert("key1", "value1");
+// hashTable.insert("key2", "value2");
+// hashTable.insert("key3", "value3");
+// console.log(current.value);
 
 module.exports = HashTable;
